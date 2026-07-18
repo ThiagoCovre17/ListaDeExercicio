fac187ebba0c0273fdcc42e67de07403198e8c22
Thiago Covre <thiago08covre@gmail.com>
2026-07-17 21:13:07 -0300

    primeiro commit

--- src/lista/Ex1.java
+++ src/lista/Ex1.java
@@ -1,4 +1,14 @@
 package lista;
 
 public class Ex1 {
+    static void imprimirLinha(int quantidade){
+        for (int i=0; i< quantidade; i++){
+            System.out.print("#");
+        }
+    }
+    public static void main(String[] args){
+        imprimirLinha(30);
+        System.out.print("\nSEJA BEM VINDO");
+        imprimirLinha(20);
+    }
 }
---
+++ src/lista/Ex2.java
@@ -0,0 +1,11 @@
+package lista;
+
+public class Ex2 {
+    void exibirSaudacao(String nome){
+        System.out.println("Seja bem vindo "+ nome +"! ao senai") ;
+
+    }
+    public static void main(String[] args){
+
+    }
+}
